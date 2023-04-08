8d10bd3174fb343196ac0550dbdfd112b661f438
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-04-08 12:55:35 +0700

    middleware-part-1

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { create, index, find, update, destroy } = require('./controller');
+const { authenticateUser, authorizeRoles } = require('../../../middleware/auth');
 
-router.get('/categories', index);
+router.get('/categories', authenticateUser, index);
 router.get('/categories/:id', find);
-router.post('/categories', create);
+router.post('/categories', authenticateUser, create);
 router.put('/categories/:id', update);
 router.delete('/categories/:id', destroy);
 
